3572d98b53216c04ff4eddb08cc2b2be5b728a40
moshangongzi <2061935864@qq.com>
2022-05-17 20:17:42 +0800

    初始化项目文件，安装了Vant

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant'
+
+import 'vant/lib/index.css'
+
+// 引入全局样式
+import './styles/index.less'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
